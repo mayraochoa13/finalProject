ac1684907f019af1631ef12bca2e92ebe059b25a
Mayra Ochoa <mayochoa@csumb.edu>
2018-05-19 03:59:47 +0000

    pls database

--- dbConnection.php
+++ dbConnection.php
@@ -1,23 +1,25 @@
 <?php   
     function connecToDB() {
-            //  $host = "localhost";
-            //  $username = "mayochoa13";
-            //  $password = "pulpO1013";
-            //  $dbname = 'myPlace'; 
+             $host = "localhost";
+             $username = "mayochoa13";
+             $password = "pulpO1013";
+             $dbname = 'myPlace'; 
+             
              //mysql://b992a7a74538f5:52272d6a@us-cdbr-iron-east-04.cleardb.net/heroku_64fb8dc07334924?reconnect=true
-        //heroku info
-          $host = "us-cdbr-iron-east-04.cleardb.net";
-          $dbname = "heroku_64fb8dc07334924";
-          $username = "b992a7a74538f5";
-          $password = "52272d6a";
+            //heroku info mysql://b992a7a74538f5:52272d6a@us-cdbr-iron-east-04.cleardb.net/heroku_64fb8dc07334924?reconnect=true
+            //   $host = "us-cdbr-iron-east-04.cleardb.net";
+            //   $dbname = "heroku_64fb8dc07334924";
+            //   $username = "b992a7a74538f5";
+            //   $password = "52272d6a";
         
-    if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
-        $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-        $host = $url["host"];
-        $dbName = substr($url["path"], 1);
-        $username = $url["user"];
-        $password = $url["pass"];
-    } 
+     $dsn = "mysql:host=$host; dbname=$dbname";
+        $opt = [
+            PDO::ATTR_ERRMODE    => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE    => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES    => false,
+        ];
+        $pdo = new PDO($dsn,$username,$password,$opt);
+        return $pdo;
     
     try {
         //Creates a database connection
@@ -30,8 +32,5 @@
        echo "Problems connecting to database!";
        exit();
     }
-    
-    
-    return $dbConn;
     }
    ?>
